c543007ecb6df5e14ac045691947268a1e4dea12
sabithbskumar <65082465+sabithbskumar@users.noreply.github.com>
2026-06-20 16:11:20 +0530

    feat: customize start page MRU section and disable icons

--- lua/plugins/start-page.lua
+++ lua/plugins/start-page.lua
@@ -12,6 +12,28 @@ return {
       }
       startify.section.header.opts.hl = "Title"
 
+      startify.section.mru_cwd.val = function()
+        local cwd = vim.fn.getcwd()
+        local fnamemodify = vim.fn.fnamemodify
+        return {
+          { type = "padding", val = 1 },
+          { type = "text", val = "[.] " .. fnamemodify(cwd, ":~"), opts = { hl = "SpecialComment", shrink_margin = false } },
+          { type = "padding", val = 1 },
+          {
+            type = "group",
+            val = function()
+              return { startify.mru(0, cwd) }
+            end,
+            opts = { shrink_margin = false },
+          },
+        }
+      end
+
+      -- disable MRU
+      startify.section.mru.val = { { type = "padding", val = 0 } }
+      -- disable file_icons
+      startify.file_icons.enabled = false
+
       alpha.setup(startify.config)
     end,
   },
